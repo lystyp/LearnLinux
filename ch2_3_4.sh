5f6c39949eb39ed3f54f66acd6a4e9289e3935fd
Daniel Shi <lystyp87015@gmail.com>
2018-06-04 00:23:46 +0800

    Add ch 2,3,4

---
+++ ch2_3_4.sh
@@ -0,0 +1,22 @@
+----------ch2-------------
+關於bios開機程序和硬體分割
+http://linux.vbird.org/linux_basic/0130designlinux.php
+http://newsletter.ascc.sinica.edu.tw/news/read_news.php?nid=1662
+
+
+---------ch3------------
+教怎麼安裝linux, 跟概念較無關，跳過吧
+
+----------ch4-------------
+關於一些說明指令的文件或查詢方法
+
+1.在終端機模式中，如果你知道某個指令，但卻忘記了相關選項與參數，請先善用 --help 的功能來查詢相關資訊；
+2.當有任何你不知道的指令或檔案格式這種玩意兒，但是你想要瞭解他，請趕快使用man或者是info來查詢！
+3.而如果你想要架設一些其他的服務，或想要利用一整組軟體來達成某項功能時，請趕快到/usr/share/doc 底下查一查有沒有該服務的說明檔喔！
+
+超簡單文書編輯器： nano
+http://linux.vbird.org/linux_basic/0160startlinux.php#nano
+
+還有提到關於關機的事項
+在文書編輯nano下面
+---------------------------
